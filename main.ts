dbcf2141418d79f206b82963b8b87f4854588133
tothjonas <54719329+PeacefulAvocado@users.noreply.github.com>
2021-09-23 12:34:35 +0200

    pxt.json, main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,50 +1,68 @@
 let num = 0
-basic.showString("Hello!")
-while (!(input.buttonIsPressed(Button.A))) {
-    num = randint(1, 5)
-    if (num == 1) {
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            . . # . .
-            . . # . .
-            . . # . .
-            `)
+for (let index = 0; index < 3; index++) {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    while (!(input.buttonIsPressed(Button.A))) {
+        num = randint(1, 5)
+        if (num == 1) {
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                . . # . .
+                . . # . .
+                . . # . .
+                `)
+        }
+        if (num == 2) {
+            basic.showLeds(`
+                . # # # .
+                . . . # .
+                . # # # .
+                . # . . .
+                . # # # .
+                `)
+        }
+        if (num == 3) {
+            basic.showLeds(`
+                . # # # .
+                . . . # .
+                . . # # .
+                . . . # .
+                . # # # .
+                `)
+        }
+        if (num == 4) {
+            basic.showLeds(`
+                . # . # .
+                . # . # .
+                . # # # .
+                . . . # .
+                . . . # .
+                `)
+        }
+        if (num == 5) {
+            basic.showLeds(`
+                . # # # .
+                . # . . .
+                . # # # .
+                . . . # .
+                . # # # .
+                `)
+        }
     }
-    if (num == 2) {
-        basic.showLeds(`
-            . # # # .
-            . . . # .
-            . # # # .
-            . # . . .
-            . # # # .
-            `)
-    }
-    if (num == 3) {
-        basic.showLeds(`
-            . # # # .
-            . . . # .
-            . . # # .
-            . . . # .
-            . # # # .
-            `)
-    }
-    if (num == 4) {
-        basic.showLeds(`
-            . # . # .
-            . # . # .
-            . # # # .
-            . . . # .
-            . . . # .
-            `)
-    }
-    if (num == 5) {
-        basic.showLeds(`
-            . # # # .
-            . # . . .
-            . # # # .
-            . . . # .
-            . # # # .
-            `)
-    }
+    basic.pause(1000)
 }
